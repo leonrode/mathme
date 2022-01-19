a183350c34e3d0dcb777025c56fa437578ea26f4
leonrode <leon.rode13@gmail.com>
2022-01-19 18:37:08 -0500

    local redirect url

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,15 @@
 import { signIn } from "next-auth/react";
 
 function Login() {
-  return <div onClick={() => signIn()}>log in</div>;
+  return (
+    <div
+      onClick={() =>
+        signIn("google", { callbackUrl: "http://localhost:3000/home" })
+      }
+    >
+      log in
+    </div>
+  );
 }
 
 export default Login;
